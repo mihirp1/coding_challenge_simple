4e18864109ebb7d1d452f2e135015619fb080aef
mihir <phatak.mihir@gmail.com>
2018-04-06 21:49:52 -0400

    Reversal Coin Problem 3 Source Code

---
+++ reversing_coins.py
@@ -0,0 +1,20 @@
+# Author: Mihir Phatak
+# Date: April 07, 2018
+# Purpose: Simple Coding Challenge at https://gist.github.com/jamalc/0d4b9229ca62d4eb9d424a736c5f75ee
+# Title : Minimum Coin Flips Calculation
+# Program Language : Python3
+
+def solution(coins):
+ count0 = coins.count(0)
+ count1 = coins.count(1)
+ 
+ if(count0 < count1):
+  return count0
+
+ else:
+  return count1
+
+if __name__ == "__main__":
+ COINS = [1,1,1,1,1,1]
+ reversal = solution(COINS) 
+ print(reversal)
